7a084d945aabb6f6f986a78e7babb1ccab403b48
nomura <paper.moon.apollo@gmail.com>
2017-12-29 16:30:37 +0900

    do task function

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 // GULP
 var gulp = require('gulp');
+var gulpIgore = require('gulp-ignore');
 var gulpwatch = require('gulp-watch');
 var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
@@ -18,7 +19,7 @@ var consoleout = require('./gulp_plugin/console_out'); // 先頭に./を記述
 // ドキュメント配下においてある前提
 const MYDOC = __dirname.replace(/\\Documents\\.+/, "/").replace(/\\/g, "/");
 const TXTS = [
-    MYDOC + 'Google ドライブ/doc/**/txt/**/*.txt'
+    MYDOC + 'Google ドライブ/doc/**/txt/**/*[0-9].txt'
 ];
 let TargetTexts_Sync = TXTS;
 
@@ -30,6 +31,16 @@ gulp.task('watch', function() {
     gulpwatch(TXTS, function(event){
         TargetTexts_Sync = event.path;
         runSequence('backup', 'test');
+
+        doTest(
+            doDist(
+                gulp.src(TargetTexts_Sync)
+                .pipe(gulpIgore.exclude(function(file){
+                    var contents = String(file.contents);
+                    return ! contents.match(/(REL:|ＲＥＬ：)/g);
+                }))
+            ), '.textlintrc_dist'
+        );
     });
 });
 
@@ -59,31 +70,46 @@ gulp.task('get_param', function(){
 })
 
 gulp.task('test', ['get_param'], function(){
-    return gulp.src(TargetTexts_Sync)
-    .pipe(plumber({
-        errorHandler: function(err) {
-//            console.log(err.messageFormatted);
-            this.emit('end');
-        }
-    }))
-    .pipe(textlint())
+    return doTest(
+        gulp.src(TargetTexts_Sync)
+    );
 });
 
 gulp.task('test_dist', ['test'], function(){
-    return gulp.src(TargetTexts_Sync)
-    .pipe(plumber({
-        errorHandler: function(err) {
-            this.emit('end');
-        }
-    }))
-    .pipe(textlint({
-        "configFile": ".textlintrc_dist"
-    }))
+    return doTest(
+        gulp.src(TargetTexts_Sync),
+        '.textlintrc_dist'
+    );
 });
 
 gulp.task('dist', ['test_dist'], function(){
-    return gulp.src(TargetTexts_Sync)
-    .pipe(textminify())
-    .pipe(gulp.dest('./dist'))
-    .pipe(exec('node node_modules//textlint//bin//textlint.js -c .textlintrc_format --fix "<%= file.path %>"'))
+    return doDist(
+        gulp.src(TargetTexts_Sync)
+    );
 });
+
+//////////////////////////////
+//DO TASK
+//////////////////////////////
+function doTest(strm, conf){
+    let setting;
+    if(conf){
+        setting = {
+            "configFile": conf
+        };
+    }
+    return strm
+        .pipe(plumber({
+            errorHandler: function(err) {
+                this.emit('end');
+            }
+        }))
+        .pipe(textlint(setting));
+}
+
+function doDist(strm){
+    return strm
+        .pipe(textminify())
+        .pipe(gulp.dest('./dist'))
+        .pipe(exec('node node_modules//textlint//bin//textlint.js -c .textlintrc_format --fix "<%= file.path %>"'));    
+}
